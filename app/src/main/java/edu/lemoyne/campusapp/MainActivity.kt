45116985aa437252a5437fa6f266f1bc8e1ecf91
Mason McManmon <ryanmason1234@icloud.com>
2026-09-25 18:08:52 -0400

    s6 done

--- app/src/main/java/edu/lemoyne/campusapp/MainActivity.kt
+++ app/src/main/java/edu/lemoyne/campusapp/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.innerShadow
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import edu.lemoyne.campusapp.ui.theme.CampusAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +29,50 @@ class MainActivity : ComponentActivity() {
         setContent {
             CampusAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Mason",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomeScreen(modifier = Modifier.padding(paddingValues = innerPadding))
                 }
             }
         }
     }
 }
-//  -- Class 5: Step 6: My Own Greeting--
+// --- Class 6 : Step one my own screen ---
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreen(modifier: Modifier = Modifier) {
+    // --- Class 6 : Step 3 - a column so things stack ---
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp)
+    ){
+        // --- Class 6 : Step 4 - Real styling ---
+        Text(
+            text = "Club Log",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "Club Events i have been to this year",
+            fontSize = 16.sp,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+
+        Text(text = "Best Member: Mason", fontSize = 18.sp)
+        Text(text = "Current Event: Website Building", fontSize = 18.sp)
+        Text(text = "Next Event: Raspberry pi", fontSize = 18.sp)
+
+    }
 }
 
-@Preview(showBackground = true)
+
+
+@Preview
 @Composable
-fun GreetingPreview() {
-    CampusAppTheme {
-        Greeting("Android")
+
+fun HomeScreenPreview() {
+    CampusAppTheme() {
+        HomeScreen()
     }
 }
